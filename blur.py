b54ea2b84cee5d2296113968cd95d79a6e39134c
Francesco De Carlo <decarlof@gmai.com>
2020-01-30 15:28:23 -0600

    update

--- blur.py
+++ blur.py
@@ -2,219 +2,110 @@
 # -*- coding: utf-8 -*-
 
 """
-Functions useful for fly1-scan
+Functions useful for fly-scan
 """
 
 from __future__ import print_function
 
 import os
+import sys
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-def calc_blur_error(exposure_time, readout_time, camera_size_x, angular_range, number_of_proj):
-    """
-    Calculate the blur error due to a rotary stage fly scan motion durng the exposure.
 
-    Parameters
-    ----------
-    exposure_time: float
-        Detector exposure time
-    readout_time : float
-        Detector read out time
-    camera_size_x : int
-        Detector X size
-    angular_range : float
-        Tomographic scan angular range
-    number_of_proj : int
-        Numember of projections
+def restricted_float(x):
 
-    Returns
-    -------
-    float
-        Blur error in pixel. For good quality reconstruction this should be < 0.2 pixel.
-    """
+    x = float(x)
+    if x < 0.0 or x > 1.0:
+        raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
+    return x
 
-    angular_step = angular_range/number_of_proj
-    scan_time = number_of_proj * (exposure_time + readout_time)
-    rot_speed = angular_range / scan_time
-    frame_rate = number_of_proj / scan_time
-    blur_delta = exposure_time * rot_speed
-    # blur_pixel = (camera_size_x / 2.0) - ((camera_size_x / 2.0) * np.cos(blur_delta * np.pi /180.))
+def calc_blur_error(exposure_time, rotation_speed, camera_size_x):
+
+    blur_delta = exposure_time * rotation_speed
     blur_pixel = (camera_size_x / 2.0) * np.sin(blur_delta * np.pi /180.)
 
-    #print("*************************************")
-    #print("Total # of proj: ", number_of_proj)
-    #print("Exposure Time: ", exposure_time, "s")
-    #print("Readout Time: ", readout_time, "s")
-    #print("Angular Range: ", angular_range, "degrees")
-    #print("Camera X size: ", camera_size_x)
-    #print("*************************************")
-    #print("Angular Step: ", angular_step, "degrees")   
-    #print("Scan Time: ", scan_time ,"s") 
-    #print("Rot Speed: ", rot_speed, "degrees/s")
-    #print("Frame Rate: ", frame_rate, "fps")
-    #print("Blur: ", blur_pixel, "pixels")
-    #print("*************************************")
-    return blur_pixel, rot_speed, scan_time
+    return blur_pixel
 
-def set_acquisition(blur_pixel, exposure_time, readout_time, camera_size_x, angular_range, number_of_proj):
 
-    """
-    Calculate frame rate and rotation speed for a desired blur error t
 
-    Parameters
-    ----------
-    blur_pixel : float
-        Desired blur error. For good quality reconstruction this should be < 0.2 pixel.
-    exposure_time: float
-        Detector exposure time
-    readout_time : float
-        Detector read out time
-    camera_size_x : int
-        Detector X size
-    angular_range : float
-        Tomographic scan angular range
-    number_of_proj : int
-        Numember of projections
+def main(arg):
 
-    Returns
-    -------
-    float
-        frame_rate, rot_speed
-    """
 
-    delta_blur  = np.arccos(((camera_size_x / 2.0) - blur_pixel) / (camera_size_x / 2.0)) * 180.0 / np.pi
-    rot_speed = delta_blur  / exposure_time
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exposure_time", nargs='?', type=float, default=0.1, help=" ")
+    parser.add_argument("--readout_time", nargs='?', type=float, default=0.006, help="flir options: 8bit: 0.006; 16-bit: 0.01")
+    parser.add_argument("--camera_size_x", nargs='?', type=int, default=2448, help="flir option: 2448")
+    parser.add_argument("--num_projections", nargs='?', type=int, default=1500, help=" ")
+    parser.add_argument("--rotation_slow_factor", nargs='?', type=restricted_float, default=1.0, help="rotation speed reduction factor from the max allowed")
 
-    scan_time = angular_range / rot_speed
-    frame_rate = number_of_proj / scan_time
-    print("*************************************")
-    print("Total # of proj: ", number_of_proj)
-    print("Exposure Time: ", exposure_time, "s")
-    print("Readout Time: ", readout_time, "s")
-    print("Angular Range: ", angular_range, "degrees")
-    print("Camera X size: ", camera_size_x)
-    print("Blur Error: ", blur_pixel, "pixels")
-    print("*************************************")
-    print("Rot Speed: : ", rot_speed, "degrees/s")
-    print("Scan Time:: ", scan_time, "s")
-    print("Frame Rate: ", frame_rate, "fps")
-    print("*************************************")
+    args = parser.parse_args()
+
+    angular_range = 180.0         # deg
+    angular_step = angular_range / args.num_projections
+
+    min_scan_time = args.num_projections * (args.exposure_time + args.readout_time)
+    max_rot_speed = angular_range / min_scan_time
+
+    rotation_speed = max_rot_speed * args.rotation_slow_factor
+    scan_time = angular_range / rotation_speed
+
+    frame_rate = args.num_projections / scan_time
+
+    blur = calc_blur_error(args.exposure_time, rotation_speed, args.camera_size_x)
+    print('Scan time: %5.2f s, Frame rate: %5.2f fps,  Rot speed: %5.2f deg/s, Blur: %5.2f pixel' % (scan_time, frame_rate, rotation_speed, blur))
+
+
+
+# def set_acquisition(blur_pixel, exposure_time, readout_time, camera_size_x, angular_range, num_projections):
+
+#     """
+#     Calculate frame rate and rotation speed for a desired blur error t
+
+#     Parameters
+#     ----------
+#     blur_pixel : float
+#         Desired blur error. For good quality reconstruction this should be < 0.2 pixel.
+#     exposure_time: float
+#         Detector exposure time
+#     readout_time : float
+#         Detector read out time
+#     camera_size_x : int
+#         Detector X size
+#     angular_range : float
+#         Tomographic scan angular range
+#     num_projections : int
+#         Numember of projections
+
+#     Returns
+#     -------
+#     float
+#         frame_rate, rot_speed
+#     """
+
+#     delta_blur  = np.arccos(((camera_size_x / 2.0) - blur_pixel) / (camera_size_x / 2.0)) * 180.0 / np.pi
+#     rot_speed = delta_blur  / exposure_time
+
+#     scan_time = angular_range / rot_speed
+#     frame_rate = num_projections / scan_time
+#     print("*************************************")
+#     print("Total # of proj: ", num_projections)
+#     print("Exposure Time: ", exposure_time, "s")
+#     print("Readout Time: ", readout_time, "s")
+#     print("Angular Range: ", angular_range, "degrees")
+#     print("Camera X size: ", camera_size_x)
+#     print("Blur Error: ", blur_pixel, "pixels")
+#     print("*************************************")
+#     print("Rot Speed: : ", rot_speed, "degrees/s")
+#     print("Scan Time:: ", scan_time, "s")
+#     print("Frame Rate: ", frame_rate, "fps")
+#     print("*************************************")
   
-    return frame_rate, rot_speed
+#     return frame_rate, rot_speed
 
-def plot2():
+   
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
-    exposure_time          = 0.1             # s
-    readout_time           = 0.05            # s
-    camera_size_x          = 4096            # pixel
-    angular_range          = 180.0           # deg
-    number_of_proj         = 6000
-
-    x = []    
-    y1 = []
-    y2 = []  
-    y3 = []  
-
-    for number_of_proj in range(100, number_of_proj, 100):
-        b_err, rot_speed, scan_time = calc_blur_error(exposure_time, readout_time, camera_size_x, angular_range, number_of_proj)
-        x.append(number_of_proj)
-        y1.append(b_err)
-        y2.append(rot_speed)
-        y3.append(scan_time)
-    
-
-    fig = plt.figure()
-    fig.suptitle('Fly scan blur error', fontsize=14, fontweight='bold')
-    fig.subplots_adjust(top=0.85)
-
-    ax = fig.add_subplot(111)
-    ax.set_xlabel('number of projections')
-    ax.set_ylabel('Blur Error (pixels)', color='red')
-    ax.plot(x, y1, 'o', color='red', label="blur error")
-    ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-    plt.legend()
-
-    scan_time_str = "%4.2f" % (scan_time/60)
-    label = 'exposure time = ' + str(exposure_time) + ' s' + '\nreadout time = ' + str(readout_time) + \
-            ' s' + '\ncamera h size = ' + str(camera_size_x) + ' pixels' + '\nangular range = ' + \
-            str(angular_range) + ' deg' + '\nscan time = ' + scan_time_str + ' min for ' + \
-            str(number_of_proj) + ' proj'
-
-    ax.text(0.8, 0.5, label,
-            verticalalignment='bottom', horizontalalignment='right',
-            transform=ax.transAxes,
-            color='black', fontsize=15)
-
-
-
-    ax1 = fig.add_subplot(111, sharex=ax, frameon=False)
-    ax1.yaxis.tick_right()
-    ax1.yaxis.set_label_position("right")
-    ax1.set_ylabel('Rotation Speed (deg/s)', color='green')
-
-    ax1.plot(x, y2, 'o', color='green', label="rotation speed")
-
-    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-    plt.show()
-
-    
-if __name__ == '__main__':
-
-    exposure_time          = 0.1             # s
-    readout_time           = 0.05            # s
-    camera_size_x          = 2048            # pixel
-    angular_range          = 180.0           # deg
-    number_of_proj         = 1500
-
-    x = []    
-    y1 = []
-    y2 = []  
-    y3 = []  
-
-    for number_of_proj in range(100, number_of_proj, 100):
-        b_err, rot_speed, scan_time = calc_blur_error(exposure_time, readout_time, camera_size_x, angular_range, number_of_proj)
-        x.append(number_of_proj)
-        y1.append(b_err)
-        y2.append(rot_speed)
-    
-
-    fig = plt.figure()
-    fig.suptitle('Fly scan blur error', fontsize=14, fontweight='bold')
-    fig.subplots_adjust(top=0.85)
-
-    ax = fig.add_subplot(111)
-    ax.set_xlabel('number of projections')
-    ax.set_ylabel('Blur Error (pixels)', color='red')
-    ax.plot(x, y1, 'o', color='red', label="blur error")
-    ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-    plt.legend()
-
-    scan_time_str = "%4.2f" % (scan_time/60)
-    label = 'exposure time = ' + str(exposure_time) + ' s' + '\nreadout time = ' + str(readout_time) + \
-            ' s' + '\ncamera h size = ' + str(camera_size_x) + ' pixels' + '\nangular range = ' + \
-            str(angular_range) + ' deg' + '\nscan time = ' + scan_time_str + ' min for ' + \
-            str(number_of_proj) + ' proj'
-
-    ax.text(0.8, 0.5, label,
-            verticalalignment='bottom', horizontalalignment='right',
-            transform=ax.transAxes,
-            color='black', fontsize=15)
-
-
-
-    # ax1 = fig.add_subplot(111, sharex=ax, frameon=False)
-    # ax1.yaxis.tick_right()
-    # ax1.yaxis.set_label_position("right")
-    # ax1.set_ylabel('Rotation Speed (deg/s)', color='green')
-    # ax1.set_yscale("log")
-    # ax1.plot(x, y2, 'o', color='green', label="rotation speed")
-
-    # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-    plt.show()
-
-    
-    
-    
-    
-    
+ 
